313fcea531662ebae6ced5d4ab8d4a108d4264af
LaineRengifo <laer.1988@gmail.com>
2025-06-24 10:40:56 +0100

    Update ALLOWED_HOSTS in settings.py for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fd0fczs)3b9#=9vhy-weawzuj@^0qc(^#_!soqi)bmdart=4n)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com']
 
 
 # Application definition
