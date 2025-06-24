43f08d902fcf7ec9f3566d0c84466b6b2342bcbf
LaineRengifo <laer.1988@gmail.com>
2025-06-24 10:51:05 +0100

    Refactor index view function in views.py for clarity

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create your views here.
-return HttpResponse("Hello, world!")
+def index(request):
+    return HttpResponse("Hello, world!")
